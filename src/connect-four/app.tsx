f3142153ccabba526b4c4124587a19ad5db75c97
Nakayama Daiju <42.daiju@gmail.com>
2021-02-23 15:02:41 +0900

    connect-four で worker 計算がマシになった

--- src/connect-four/app.tsx
+++ src/connect-four/app.tsx
@@ -1,21 +1,41 @@
 import React, {useEffect, useState, useMemo, useReducer} from 'react';
-import * as Comlink from 'comlink';
+import {wrap} from 'comlink';
 
 import {Svg, Cross, Circle, Square} from '../svg';
 import {ResetButton} from '../button';
 import {Select} from '../select';
+import {ModuleType} from './worker';
 
-const wasm = Comlink.wrap<import('./worker').ModuleType>(new Worker('./worker', {
-  name: 'connect-four',
-  type: 'module'
-}));
 
-function useWasm(): boolean {
-  const [loaded, setLoaded] = useState(false);
+function createWorker(): Worker {
+  return new Worker('./worker', {
+    name: 'connect-four',
+    type: 'module'
+  });
+}
+
+function useWorker(): [boolean, (calculating: boolean) => void, ModuleType, () => void] {
+  const [worker, setWorker] = useState(createWorker);
+  const proxy = useMemo(() => wrap<ModuleType>(worker), [worker]);
+  const [initialized, setInitialize] = useState(false);
+  const [calculating, setCalculating] = useState(true);
+
   useEffect(() => {
-    wasm.initialize().then(() => setLoaded(true));
-  }, [loaded]);
-  return loaded;
+    if (initialized) return;
+    proxy.initialize().then(() => {
+      setInitialize(true);
+      setCalculating(false);
+    });
+  }, [initialized, calculating, proxy]);
+
+  function cancel(): void {
+    worker.terminate();
+    setCalculating(true);
+    setWorker(createWorker());
+    setInitialize(false);
+  }
+
+  return [calculating, setCalculating, proxy, cancel];
 }
 
 type Side = 'A' | 'B';
@@ -100,20 +120,8 @@ interface SearchResponse {
   readonly score: string
 }
 
-type State =
-  | 'queue_input'
-  | 'wait_for_input'
-  | 'queue_judge'
-  | 'wait_for_judge'
-  | 'end';
-
 interface HumanPlayer {
   readonly type: 'Human'
-  readonly name: string
-}
-
-interface ABSearchPlayer {
-  readonly type: 'ABSearch'
   readonly name: string
 }
 
@@ -127,23 +135,75 @@ interface McTreePlayer {
 
 type Player =
   | HumanPlayer
-  | ABSearchPlayer
   | McTreePlayer;
 
+type Winner = Side | 'F' | null;
+
+interface ConnectFourState {
+  board: BoardState
+  key: number
+  winner: Winner
+  judged: boolean
+  player: { [P in Side]: number }
+}
+
+type Action =
+  | { type: 'reset' }
+  | { type: 'change_player', side: Side, id: number }
+  | { type: 'put', key: number, side: Side, position: number, score?: string }
+  | { type: 'judge', key: number, winner: Winner }
+  ;
+
+function init(player: { [P in Side]: number }): ConnectFourState {
+  return {
+    board: {
+      cols: [[], [], [], [], [], [], []],
+      next: 'A',
+      history: [],
+    },
+    key: Math.random(),
+    winner: null,
+    judged: true,
+    player,
+  };
+}
+
+function reducer(state: ConnectFourState, action: Action): ConnectFourState {
+  switch (action.type) {
+    case 'reset':
+      return init(state.player);
+    case 'change_player':
+      if (state.player[action.side] === action.id) return state;
+      return init({...state.player, [action.side]: action.id});
+    case 'put':
+      if (action.key !== state.key) return state;
+      if (state.board.next !== action.side) return state;
+      if (state.winner) return state;
+      if (!state.judged) return state;
+      if (state.board.cols[action.position].length >= 6) {
+        console.warn(state, action);
+        return state;
+      }
+      return {
+        ...state,
+        board: put(state.board, action.position, action.score),
+        judged: false,
+      };
+    case 'judge':
+      if (action.key !== state.key) return state;
+      return {
+        ...state,
+        winner: action.winner,
+        judged: true,
+      };
+  }
+}
+
 function ConnectFour(): React.ReactElement {
-  const defaultBoard: BoardState = {
-    cols: [[], [], [], [], [], [], []],
-    next: 'A',
-    history: [],
-  } as const;
   const playerMaster = useMemo<ReadonlyArray<Player>>(() => [
     {
       type: 'Human',
       name: 'Human',
-    },
-    {
-      type: 'ABSearch',
-      name: 'Alpha-Beta Search'
     },
     {
       type: 'MCTree',
@@ -161,104 +221,73 @@ function ConnectFour(): React.ReactElement {
     },
   ], []);
 
-  const loaded = useWasm();
-  const [state, setState] = useState<State>('wait_for_input');
-  const [winner, setWinner] = useState<Side | 'F' | null>(null);
-  const [board, setBoard] = useState<BoardState>(defaultBoard);
-  const [player, setPlayer] = useState({A: 0, B: 0});
+  const [calculating, setCalculating, wasm, cancel] = useWorker();
+  const [state, dispatch] = useReducer(reducer, init({A: 0, B: 0}));
 
   useEffect(() => {
-    if (!loaded) return;
-
-    switch (state) {
-      case 'queue_input': {
-        setState('wait_for_input');
-        const p = playerMaster[player[board.next]];
-        switch (p.type) {
-          case 'Human': {
-            break;
-          }
-          case 'ABSearch': {
-            wasm.search({cols: board.cols}).then((result: SearchResponse) => {
-              if (typeof result.position === 'number') {
-                setBoard(put(board, result.position, result.score));
-                setState('queue_judge');
+    if (calculating) return;
+    if (state.winner) return;
+    if (state.judged) {
+      const side = state.board.next;
+      const player = playerMaster[state.player[side]];
+      switch (player.type) {
+        case 'Human':
+          break;
+        case 'MCTree':
+          const key = state.key;
+          setCalculating(true);
+          wasm.mctree({cols: state.board.cols}, player.limit, player.expansion_threshold, player.c)
+            .then((r: SearchResponse) => {
+              if (typeof r.position === 'number') {
+                dispatch({type: 'put', key, side, position: r.position, score: r.score});
               } else {
-                setState('end');
+                console.error(r);
               }
+              setCalculating(false);
             });
-            break;
-          }
-          case 'MCTree': {
-            wasm.mctree({cols: board.cols}, p.limit, p.expansion_threshold, p.c).then((result: SearchResponse) => {
-              if (typeof result.position === 'number') {
-                setBoard(put(board, result.position, result.score));
-                setState('queue_judge');
-              } else {
-                setState('end');
-              }
-            });
-          }
-        }
-        break;
+          break;
       }
-      case 'queue_judge':
-        setState('wait_for_judge');
-        wasm.calculateWinner({cols: board.cols}).then((w: Side | 'F' | null) => {
-          setWinner(w);
-          if (w) {
-            setState('end');
-          } else {
-            setState('queue_input');
-          }
-        });
-        break;
-      case 'wait_for_input':
-      case 'wait_for_judge':
-      case 'end':
-        break;
+    } else {
+      setCalculating(true);
+      const key = state.key;
+      wasm.calculateWinner({cols: state.board.cols}).then((winner: Winner) => {
+        dispatch({type: 'judge', key, winner});
+        setCalculating(false);
+      });
     }
-  }, [loaded, state, player, playerMaster, board]);
 
-  if (!loaded) {
-    return <div>Loading...</div>;
-  }
+  }, [calculating, playerMaster, state, setCalculating, wasm]);
 
   function handleClick(i: number): void {
-    if (state !== 'wait_for_input') return;
-    if (board.cols[i].length >= 6) return;
-    if (playerMaster[player[board.next]].name === 'Human') {
-      setBoard(put(board, i));
-      setState('queue_judge');
+    if (state.board.cols[i].length >= 6) return;
+    const side = state.board.next;
+    if (playerMaster[state.player[side]].type === 'Human') {
+      dispatch({type: 'put', key: state.key, side, position: i});
     }
   }
 
-  function handlePlayerChange(side: 'A' | 'B', i: number): void {
-    if (player[side] !== i) {
-      setPlayer({
-        ...player,
-        [side]: i,
-      });
-      resetBoard();
+  function handlePlayerChange(side: 'A' | 'B', id: number): void {
+    if (state.player[side] !== id) {
+      cancel();
+      dispatch({type: 'change_player', side, id});
     }
   }
 
   function resetBoard(): void {
-    setBoard(defaultBoard);
-    setWinner(null);
-    setState('queue_input');
+    cancel();
+    dispatch({type: 'reset'});
   }
 
   let status;
-  if (winner === null) {
-    status = `next player: ${board.next}`;
-  } else if (winner === 'F') {
+  if (state.winner === null) {
+    status = `next player: ${state.board.next}`;
+  } else if (state.winner === 'F') {
     status = 'draw';
   } else {
-    status = `winner: ${winner}`
+    status = `winner: ${state.winner}`
   }
 
-  const history = board.history.map((h, i) => {
+  const history = state.board.history.map((h, i) => {
     if (h.score != null) {
       return <li key={i}>{'AB'[i % 2]}: pos={h.position} score={h.score}</li>
     } else {
@@ -272,28 +301,28 @@ function ConnectFour(): React.ReactElement {
         X
         <Select
           items={playerMaster.map((p) => p.name)}
-          selected={player.A}
+          selected={state.player.A}
           isDisabled={false}
           onChange={(i) => handlePlayerChange('A', i)}
         />
         vs
         <Select
           items={playerMaster.map((p) => p.name)}
-          selected={player.B}
+          selected={state.player.B}
           isDisabled={false}
           onChange={(i) => handlePlayerChange('B', i)}
         />
         O
       </div>
       <div className="content">
-        <Board onClick={handleClick} cols={board.cols}/>
+        <Board onClick={handleClick} cols={state.board.cols}/>
       </div>
       <div className="content">
         <p>{status}</p>
-        <p>{state}</p>
+        <p>calculating: {calculating ? 't' : 'f'}</p>
       </div>
       <div className="content">
-        <ResetButton hasWinner={winner !== null} onClick={resetBoard}/>
+        <ResetButton hasWinner={state.winner !== null} onClick={resetBoard}/>
       </div>
       <div className="content">
         <ol>{history}</ol>
